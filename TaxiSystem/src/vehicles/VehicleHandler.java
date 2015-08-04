9963b2829da9670b407a2ecf43a638a52d623caf
Priya <Priya@10.189.155.146>
2015-08-04 15:13:46 -0700

    Vehicle Handler has been modified

--- TaxiSystem/src/vehicles/VehicleHandler.java
+++ TaxiSystem/src/vehicles/VehicleHandler.java
@@ -2,15 +2,17 @@ package vehicles;
 
 public class VehicleHandler {
 
-//
-//	public static void main(String[] args) {
-//		Vehicle vehicle = new Vehicle();
-//		vehicle.becomeAvailable();
-//		vehicle.doMaintenance();
-//		vehicle.becomeAvailable();
-//		vehicle.becomeAvailable();
-//		vehicle.hireVehicle();
-//		vehicle.becomeAvailable();
-//	}
 
+	public static void main(String[] args) {
+		Vehicle vehicle = new Vehicle();
+		vehicle.becomeAvailable();
+		vehicle.doMaintenance();
+		vehicle.becomeAvailable();
+		vehicle.becomeAvailable();
+		vehicle.hireVehicle();
+		vehicle.becomeAvailable();
+	}
+
+	
+	// Added By Priya 
 }
